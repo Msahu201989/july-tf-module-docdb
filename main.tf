4ef4f4935681166238128fd9c98f5c8ac0d4c4e6
Mukesh <Stunty@007>
2023-07-27 22:30:05 +0530

    July 23 learning devops

--- main.tf
+++ main.tf
@@ -1,8 +1,26 @@
+resource "aws_docdb_subnet_group" "default" {
+  name                    = "${var.env}-${var.name}-roboshop-docdb"
+  subnet_ids              = var.subnets
+
+  tags = {
+    Name                  = "${var.env}-${var.name}-roboshop-docdb"
+  }
+}
+
 resource "aws_docdb_cluster" "docdb" {
-  for_each                =  var.docdb
-  cluster_identifier      = "${var.env}-roboshop-docdb"
-  engine                  =  each.value.engine
-  master_username         = "admin1"
-  master_password         = "mustbeeightchars"
-  skip_final_snapshot     = true
+  depends_on              = [aws_docdb_subnet_group.default]
+  cluster_identifier      = "${var.env}-${var.name}-roboshop-docdb"
+  engine                  =  var.engine
+  master_username        = local.DOCDB_USER
+  master_password        = local.DOCDB_PASS
+  skip_final_snapshot     = var.skip_final_snapshot
+  db_subnet_group_name    = "${var.env}-${var.name}-roboshop-docdb"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  for_each           = var.nodes
+  identifier         = "${var.env}-${var.name}-roboshop-docdb-${each.key}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = each.value.instance_class
 }
